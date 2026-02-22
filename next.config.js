d7df5dca3f0958a0aa190cf709598af3639b05c0
LarryR <larryarbot@gmail.com>
2026-02-22 14:57:40 -0800

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/lorem-ipsum-generator',
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
